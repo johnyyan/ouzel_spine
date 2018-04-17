0c399ad6a60f3e453a7973a5cb9f5875ed8aaafe
yanweicai <yanweicai@moqihuyu.com>
2018-04-17 10:41:35 +0800

    lua binding

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 
 #include "SpineSample.hpp"
 
+#define SOL_CHECK_ARGUMENTS 1
+#include <sol/sol.hpp>
+
 std::string DEVELOPER_NAME = "elnormous";
 std::string APPLICATION_NAME = "Spine sample";
 
@@ -10,6 +13,15 @@ std::unique_ptr<SpineSample> sample;
 void ouzelMain(const std::vector<std::string>& args)
 {
     OUZEL_UNUSED(args);
+    
+    //lua state
+    sol::state lua;
+    lua.open_libraries(sol::lib::base, sol::lib::coroutine);
 
-    sample.reset(new SpineSample());
+    int x = 0;
+    lua.set_function("beep", [&x]{ ++x; });
+    lua.script("beep()");
+    lua.script("print('dddd')");
+    
+//    sample.reset(new SpineSample());
 }
